502a83d3bb75957195b12e5f7e26d619bf8187fe
smajaas <smajaas@gmail.com>
2023-04-03 22:09:09 +0530

    page structure

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Dashboard from '../pages/Dashboard';
+import Error from '../pages/Error';
+import Register from '../pages/Register';
+import Landing from '../pages/Landing';
+
+export { Dashboard, Error, Register, Landing };
